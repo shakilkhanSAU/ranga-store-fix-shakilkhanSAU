a7f74f7ffdc10d2f6000735dc85f8d826f9fa983
shakilkhanSAU <shakilkhan.sau@gmail.com>
2021-09-13 00:54:02 +0600

    fandamental error fixed

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
 const loadProducts = () => {
-  const url = `https://fakestoreapi.com/products`;
-  fetch(url)
+  fetch('https://fakestoreapi.com/products')
     .then((response) => response.json())
     .then((data) => showProducts(data));
 };
@@ -8,17 +7,22 @@ loadProducts();
 
 // show all product in UI 
 const showProducts = (products) => {
+  console.log(products)
   const allProducts = products.map((pd) => pd);
   for (const product of allProducts) {
-    const image = product.images;
+    console.log(product)
+    const rating = product.rating.rate;
+    const ratingCount = product.rating.count;
+    const image = product.image;
     const div = document.createElement("div");
     div.classList.add("product");
     div.innerHTML = `<div class="single-product">
       <div>
-    <img class="product-image" src=${image}></img>
+      <img class="product-image" src="${image}"></img>
       </div>
       <h3>${product.title}</h3>
       <p>Category: ${product.category}</p>
+      <p> Rating: ${rating}<span> Total Review: ${ratingCount}</p>
       <h2>Price: $ ${product.price}</h2>
       <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
       <button id="details-btn" class="btn btn-danger">Details</button></div>
@@ -30,14 +34,15 @@ let count = 0;
 const addToCart = (id, price) => {
   count = count + 1;
   updatePrice("price", price);
-
+  // calling updateTotal Price
+  updateTotal();
   updateTaxAndCharge();
   document.getElementById("total-Products").innerText = count;
 };
 
 const getInputValue = (id) => {
   const element = document.getElementById(id).innerText;
-  const converted = parseInt(element);
+  const converted = parseFloat(element);
   return converted;
 };
 
@@ -46,7 +51,8 @@ const updatePrice = (id, value) => {
   const convertedOldPrice = getInputValue(id);
   const convertPrice = parseFloat(value);
   const total = convertedOldPrice + convertPrice;
-  document.getElementById(id).innerText = Math.round(total);
+  const totalFloat = parseFloat(total)
+  document.getElementById(id).innerText = totalFloat.toFixed(2);
 };
 
 // set innerText function
